eaf91decc8c8c4f8dad5aafcb60db1530f9c53b5
ENG_BXI <eng.0.bxi@gmail.com>
2025-03-18 19:39:17 +0300

    show all blocks to home page

--- src/Layouts/DashBoard/Building/Building.jsx
+++ src/Layouts/DashBoard/Building/Building.jsx
@@ -1,41 +1,55 @@
-import React, {useRef} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import './Building.css';
-import {Neighborhood_1, Neighborhood_1_Person, Neighborhood_2, Neighborhood_2_Person, Neighborhood_3, Neighborhood_3_Person} from '../../../Assets/Images';
+import {Neighborhood_1, Neighborhood_1_Person} from '../../../Assets/Images';
 import NeighborhoodCard from '../../../Components/NeighborhoodCard';
-import { useNavigate } from 'react-router';
+import {useNavigate} from 'react-router';
+import axios from 'axios';
+import {BASEURL, GET_ALL_BLOCK} from '../../../Api/EndPoint';
 
-let neighborhoodData = [
-  {Image: Neighborhood_1, Neighborhood: 'حي المتضررين', NeighborhoodPersonProfile: Neighborhood_1_Person, PersonName: 'عبدالله سالم', NumberOfFamily: '210'},
-  {Image: Neighborhood_2, Neighborhood: 'حي الانشاءات', NeighborhoodPersonProfile: Neighborhood_2_Person, PersonName: 'منير عبدالرؤوف', NumberOfFamily: '307'},
-  {Image: Neighborhood_3, Neighborhood: 'حي المساكن', NeighborhoodPersonProfile: Neighborhood_3_Person, PersonName: 'اسماعيل زكريا', NumberOfFamily: '654'}
-];
+// let neighborhoodData = [
+//   {Image: Neighborhood_1, Neighborhood: 'حي المتضررين', NeighborhoodPersonProfile: Neighborhood_1_Person, PersonName: 'عبدالله سالم', NumberOfFamily: '210'},
+//   {Image: Neighborhood_2, Neighborhood: 'حي الانشاءات', NeighborhoodPersonProfile: Neighborhood_2_Person, PersonName: 'منير عبدالرؤوف', NumberOfFamily: '307'},
+//   {Image: Neighborhood_3, Neighborhood: 'حي المساكن', NeighborhoodPersonProfile: Neighborhood_3_Person, PersonName: 'اسماعيل زكريا', NumberOfFamily: '654'}
+// ];
 const Building = () => {
   let ToggleButton = useRef(null);
   let nav = useNavigate();
+  let [neighborhoodData, setNeighborhoodData] = useState([]);
   function handleToggle() {
     ToggleButton.current.classList.toggle('show');
   }
+  useEffect(() => {
+    axios.get(`${BASEURL}/${GET_ALL_BLOCK}`).then(data => { setNeighborhoodData(data.data.data); console.log(data);
+    });
+  }, []);
   return (
     <div className='building'>
-      <div className='d-flex align-items-center column-gap-3 my-4'>
+      <div className='d-flex align-items-center gap-3 my-4'>
         <h2 className='mb-0'>الوحدات السكنية</h2>
         <div className='dropdown'>
-          <button onClick={handleToggle} className='toggle d-flex align-items-center justify-content-center dropdown-toggle'>+</button>
+          <button onClick={handleToggle} className='toggle d-flex align-items-center justify-content-center dropdown-toggle'>
+            +
+          </button>
           <div ref={ToggleButton} className='dropdown-menu text-center'>
-            <button onClick={()=>nav("/add-new-square")} className='btn btn-sm border-0'> اضافة مربع سكني </button>
+            <button onClick={() => nav('/add-new-square')} className='btn btn-sm border-0'>
+              {' '}
+              اضافة مربع سكني{' '}
+            </button>
             <div className='dropdown-divider'></div>
-            <button onClick={()=>nav("/add-new-family")} className='btn btn-sm border-0'> اضافة اسرة</button>
+            <button onClick={() => nav('/add-new-family')} className='btn btn-sm border-0'>
+              {' '}
+              اضافة اسرة
+            </button>
             <div className='dropdown-divider'></div>
-            <button onClick={()=>nav("/add-new-person")} className='btn btn-sm border-0'> اضافة شخص</button>
+            <button onClick={() => nav('/add-new-person')} className='btn btn-sm border-0'>
+              {' '}
+              اضافة شخص
+            </button>
           </div>
         </div>
       </div>
       {/* <PlaceInLine /> */}
-      <div className='d-flex column-gap-2'>
-        {neighborhoodData.map((element, index) => (
-          <NeighborhoodCard key={index} Neighborhood={element.Neighborhood} NeighborhoodPersonProfile={element.NeighborhoodPersonProfile} NumberOfFamily={element.NumberOfFamily} PersonName={element.PersonName} Image={element.Image} />
-        ))}
-      </div>
+      <div className='d-flex flex-wrap column-gap-2'>{neighborhoodData.length > 0 && neighborhoodData.map((element, index) => <NeighborhoodCard key={index} Neighborhood={element.name} NeighborhoodPersonProfile={Neighborhood_1_Person} NumberOfFamily={element.families.length  || "لاتوجد "} PersonName='عبدالله سالم' Image={Neighborhood_1} />)}</div>
     </div>
   );
 };
